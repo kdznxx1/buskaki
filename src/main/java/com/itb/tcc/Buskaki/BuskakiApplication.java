d6f6e2fed6356dfc3c1a1a8216e5e85f1d6ea09c
Carlos Eduardo <ph982677957@gmail.com>
2023-08-24 21:46:09 -0300

    Criação do ambiente de desenvolvimento

---
+++ src/main/java/com/itb/tcc/Buskaki/BuskakiApplication.java
@@ -0,0 +1,13 @@
+package com.itb.tcc.Buskaki;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BuskakiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BuskakiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/itb/tcc/Buskaki/BuskakiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.itb.tcc.Buskaki;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BuskakiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
